49fd1680845aed19e37660111d5bcd9d55030d75
George S <georgesimbajon28@gmail.com>
2021-03-27 00:49:11 -0700

    Update AddRemove.java

--- AddRemove.java
+++ AddRemove.java
@@ -14,7 +14,7 @@ public class AddRemove {
         System.out.println(courses);
     }
 
-    public void addCourse() {
+    public ArrayList<String> addCourse() {
         System.out.print("Would you like to add a course? (y/n): ");
         String ans = stdin.next();
         while (ans.equals("y")){
@@ -23,21 +23,27 @@ public class AddRemove {
             System.out.print("Would you still like to add a course? (y/n): ");
             ans = stdin.next();
         }
+
+        return courses;
     }
 
-    public void removeCourse(){
-        System.out.print("would you like to remove a course? (y/n): ");
+    public ArrayList<String> removeCourse(){
+        System.out.print("Would you like to remove a course? (y/n): ");
         String ans = stdin.next();
 
-        if (ans.equals("y")  && courses.size() != 0 ){
+        if (ans.equals("y") && courses.size() != 0 ){
             while (ans.equals("y")){
                 System.out.println("Current courses: " + courses);
                 System.out.print("Pick a course you would ike to remove (Starting at index 0): ");
-                courses.remove(stdin.next());
+                courses.remove(stdin.nextInt());
                 System.out.print("Would you like to remove another class? (y/n): ");
                 ans = stdin.next();
             }
         }
-        else{System.out.println("You have no courses to remove!");}
+        else {
+            System.out.println("You have no courses to remove!");
+        }
+
+        return courses;
     }
 }
