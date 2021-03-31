7fe1e2133f6d722a5a37687aa46c8784f94404e8
George S <georgesimbajon28@gmail.com>
2021-03-31 16:20:20 -0700

    Add files via upload

--- AddRemove.java
+++ AddRemove.java
@@ -1,48 +1,49 @@
 import java.util.*;
 
+/**
+ * Adds the ability to add and remove classes from a student or teacher's list of courses.
+ */
 public class AddRemove {
-    private Scanner stdin = new Scanner(System.in);
-    private Student student;
-    private ArrayList<String> courses;
+    private static Scanner stdin = new Scanner(System.in);
 
-    public AddRemove(Student student){
-        this.student = student;
-        courses = this.student.getClasses();
-    }
+    /**
+     * Adds a class to a SchoolMember's list of classes.
+     * @param member Accepts a SchoolMember to add courses to.
+     * @return An updated ArrayList of courses with the added course.
+     */
+    public static ArrayList<String> addCourse(SchoolMember member) {
+        ArrayList<String> courses = member.getCourses();
 
-    public void printClasses() {
-        System.out.println(courses);
-    }
-
-    public ArrayList<String> addCourse() {
-        System.out.print("Would you like to add a course? (y/n): ");
-        String ans = stdin.next();
-        while (ans.equals("y")){
-            System.out.print("Enter the class you want to add: ");
+        String ans;
+        do {
+            System.out.print("Enter course you would like to add: ");
             courses.add(stdin.next());
             System.out.print("Would you still like to add a course? (y/n): ");
             ans = stdin.next();
-        }
+            if(ans.equals("n"))
+                break;
+        } while(ans.equals("y"));
 
         return courses;
     }
 
-    public ArrayList<String> removeCourse(){
-        System.out.print("Would you like to remove a course? (y/n): ");
-        String ans = stdin.next();
+    /**
+     * Removes a class to a SchoolMember's list of classes.
+     * @param member Accepts a SchoolMember to remove a course from.
+     * @return An updated ArrayList of courses with the removed course.
+     */
+    public static ArrayList<String> removeCourse(SchoolMember member){
+        ArrayList<String> courses = member.getCourses();
 
-        if (ans.equals("y") && courses.size() != 0 ){
-            while (ans.equals("y")){
-                System.out.println("Current courses: " + courses);
-                System.out.print("Pick a course you would ike to remove (Starting at index 0): ");
-                courses.remove(stdin.nextInt());
-                System.out.print("Would you like to remove another class? (y/n): ");
-                ans = stdin.next();
-            }
-        }
-        else {
-            System.out.println("You have no courses to remove!");
-        }
+        String ans;
+        do {
+            System.out.print("Enter course you would like to remove (Staring from index 0): ");
+            courses.remove(stdin.nextInt());
+            System.out.println("Would you still like to remove a course? (y/n)");
+            ans = stdin.next();
+            if(ans.equals("n"))
+                break;
+        } while(ans.equals("y"));
 
         return courses;
     }
